ab01dd8a91cb352be01eee147724f9ee5f4b3370
Nguyễn Thị Hà <hantctk42@gmail.com>
2022-10-05 10:52:05 +0700

    Add Todo 🍑

---
+++ src/redux/reducer.js
@@ -0,0 +1,34 @@
+const initState = {
+  filters: {
+    search: "",
+    status: "All",
+    priority: [],
+  },
+
+  todoList: [
+    { id: 1, name: "Learn Yoga", completed: false, priority: "Medium" },
+    { id: 2, name: "Learn React", completed: true, priority: "High" },
+    { id: 3, name: "Learn Redux", completed: false, priority: "Low" },
+  ],
+};
+
+const rootReducer = (state = initState, action) => {
+  /**
+   * {
+   *    type: 'todoList/addTodo',
+   *    payload: {id:3, name:'Learn Redux', completed: false, priority: 'Low'}
+   * }
+   */
+  switch (action.type) {
+    case "todoList/addTodo":
+      return {
+        ...state,
+        todoList: [...state.todoList, action.payload],
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
